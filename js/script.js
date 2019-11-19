357baf568a60ae758aa49648cdc32f92af30c6ff
tacostahern <35584612+tacostahern@users.noreply.github.com>
2019-11-19 15:56:08 -0600

    added some variables for button and textinput

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 function init(){
 //add your javascrip between these two lines of code
- 
-
-
-
-
-
-
-
+  var input = document.getElementById("entryinput")
+  var button = document.getElementById("entrybutton")
+  
+}
 window.addEventListener('load', init);
